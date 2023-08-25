2dd3c510c4168a45f7ad2d6548c3adedc546faf5
corjeong <corjeong@gmail.com>
2023-08-25 15:22:45 +0900

    20230825 프로젝트 완성

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,190 @@
+import { Component } from './components/component.js';
+import {
+  InputDialog,
+  MediaData,
+  TextData,
+} from './components/dialog/dialog.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
+import { TextSectionInput } from './components/dialog/input/text-input.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from './components/page/page.js';
 
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
 class App {
-  private readonly page: PageComponent;
-  constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+  private readonly page: Component & Composable;
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      'Image Title',
-      'https://picsum.photos/600/300'
+    // const image = new ImageComponent(
+    //   'Image Title',
+    //   'https://picsum.photos/600/300'
+    // );
+    // // image.attachTo(appRoot, 'beforeend');
+    // this.page.addChild(image);
+
+    // const video = new VideoComponent(
+    //   'Video Title',
+    //   'https://youtu.be/RqqjuBLDXKk'
+    // );
+    // // video.attachTo(appRoot, 'beforeend');
+    // this.page.addChild(video);
+
+    // const note = new NoteComponent('Note Title', 'Note Body');
+    // // note.attachTo(appRoot, 'beforeend');
+    // this.page.addChild(note);
+
+    // const todo = new TodoComponent('Todo Title', 'Todo Item');
+    // // todo.attachTo(appRoot, 'beforeend');
+    // this.page.addChild(todo);
+
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-image',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-video',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-note',
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-todo',
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+
+    // const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+    // imageBtn.addEventListener('click', () => {
+    //   const dialog = new InputDialog();
+    //   const inputSection = new MediaSectionInput();
+    //   dialog.addChild(inputSection);
+    //   dialog.attachTo(this.dialogRoot);
+
+    //   dialog.setOnCloseListener(() => {
+    //     dialog.removeFrom(this.dialogRoot);
+    //   });
+    //   dialog.setOnSubmitListener(() => {
+    //     //섹션을 만들어서 페이지에 추가 해준다
+    //     const image = new ImageComponent(inputSection.title, inputSection.url);
+    //     this.page.addChild(image);
+    //     dialog.removeFrom(this.dialogRoot);
+    //   });
+    // });
+
+    // const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
+    // videoBtn.addEventListener('click', () => {
+    //   const dialog = new InputDialog();
+    //   const inputSection = new MediaSectionInput();
+    //   dialog.addChild(inputSection);
+    //   dialog.attachTo(this.dialogRoot);
+
+    //   dialog.setOnCloseListener(() => {
+    //     dialog.removeFrom(this.dialogRoot);
+    //   });
+    //   dialog.setOnSubmitListener(() => {
+    //     //섹션을 만들어서 페이지에 추가 해준다
+    //     const video = new VideoComponent(inputSection.title, inputSection.url);
+    //     this.page.addChild(video);
+    //     dialog.removeFrom(this.dialogRoot);
+    //   });
+    // });
+
+    //   const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
+    //   noteBtn.addEventListener('click', () => {
+    //     const dialog = new InputDialog();
+    //     const inputSection = new TextSectionInput();
+    //     dialog.addChild(inputSection);
+    //     dialog.attachTo(this.dialogRoot);
+
+    //     dialog.setOnCloseListener(() => {
+    //       dialog.removeFrom(this.dialogRoot);
+    //     });
+    //     dialog.setOnSubmitListener(() => {
+    //       //섹션을 만들어서 페이지에 추가 해준다
+    //       const note = new NoteComponent(inputSection.title, inputSection.body);
+    //       this.page.addChild(note);
+    //       dialog.removeFrom(this.dialogRoot);
+    //     });
+    //   });
+    //   const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
+    //   todoBtn.addEventListener('click', () => {
+    //     const dialog = new InputDialog();
+    //     const inputSection = new TextSectionInput();
+    //     dialog.addChild(inputSection);
+    //     dialog.attachTo(this.dialogRoot);
+
+    //     dialog.setOnCloseListener(() => {
+    //       dialog.removeFrom(this.dialogRoot);
+    //     });
+    //     dialog.setOnSubmitListener(() => {
+    //       //섹션을 만들어서 페이지에 추가 해준다
+    //       const todo = new TodoComponent(inputSection.title, inputSection.body);
+    //       this.page.addChild(todo);
+    //       dialog.removeFrom(this.dialogRoot);
+    //     });
+    //   });
+
+    // For demo :)
+    this.page.addChild(
+      new ImageComponent('Image Title', 'https://picsum.photos/800/400')
+    );
+    this.page.addChild(
+      new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0')
+    );
+    this.page.addChild(
+      new NoteComponent('Note Title', "Don't forget to code your dream")
+    );
+    this.page.addChild(new TodoComponent('Todo Title', 'TypeScript Course!'));
+    this.page.addChild(
+      new ImageComponent('Image Title', 'https://picsum.photos/800/400')
+    );
+    this.page.addChild(
+      new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0')
+    );
+    this.page.addChild(
+      new NoteComponent('Note Title', "Don't forget to code your dream")
+    );
+    this.page.addChild(new TodoComponent('Todo Title', 'TypeScript Course!'));
+  }
+
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const input = new InputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(this.dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        //섹션을 만들어서 페이지에 추가 해준다
+        const image = makeSection(input);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
+      });
+    });
   }
 }
 
-new App(document.querySelector('.document')! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement, document.body);
---
+++ src/components/page/item/video.ts
@@ -0,0 +1,49 @@
+import { BaseComponent } from '../../component.js';
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="page-item__title video__title"></h3>
+        </section>`);
+
+    const iframe = this.element.querySelector(
+      '.video__iframe'
+    )! as HTMLIFrameElement;
+    iframe.src = this.convertToEmbeddedURL(url); //url -> videoId -> embed
+
+    const titleElement = this.element.querySelector(
+      '.video__title'
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
+  }
+}
+
+// input
+// https://www.youtube.com/watch?v=K3-jG52XwuQ
+// https://youtu.be/K3-jG52XwuQ
+// output
+// https://www.youtube.com/embed/K3-jG52XwuQ
+// 정규표현식 Regex
+
+// <iframe
+//   width='770'
+//   height='433'
+//   src='https://www.youtube.com/embed/RqqjuBLDXKk'
+//   title='JIHYO "Killin&#39; Me Good" Performance Video'
+//   frameborder='0'
+//   allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+//   allowfullscreen
+// ></iframe>;
